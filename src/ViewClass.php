34b48d3c5574a542254c26bf075fad4426fbec3c
DQNEO <dqneoo@gmail.com>
2016-02-07 13:59:35 +0900

    gc

--- src/ViewClass.php
+++ src/ViewClass.php
@@ -71,17 +71,6 @@ class Ethna_ViewClass
     /** @protected    boolean  配列フォームを呼んだカウンタをリセットするか否か */
     protected $reset_counter = false;
 
-    /** @var  boolean  デフォルトのヘッダ出力を使用するか否か  */
-    /**                ヘッダ出力を改造する場合はfalseにする   */
-    public $has_default_header = true;
-
-    /** @var  array    default header */
-    public $default_header = array(
-        'Pragma' => 'no-cache',
-        'Cache-Control' => 'no-cache, no-store, must-revalidate',
-    );
-
-    // {{{ Ethna_ViewClass
     /**
      *  Ethna_ViewClassのコンストラクタ
      *
@@ -154,78 +143,6 @@ class Ethna_ViewClass
             throw new \Exception($e->getMessage());
         }
     }
-    // }}}
-
-    // {{{ header
-    /**
-     *  HTTPヘッダを送信します。
-     *
-     *  @param  mixed   ヘッダを設定する値
-     *                  配列指定の場合、header => value の形式
-     *                  整数指定の場合は、HTTPステータスコード
-     *                  文字列で指定する場合は、ヘッダ出力をそのまま指定
-     *  @access public
-     */
-    public function header($status)
-    {
-        if (is_array($status)) {
-            foreach ($status as $key => $status) {
-                header ($key . ": " . $status);
-            }
-        } else if (is_int($status)) {
-            $codes = array(
-                100 => "Continue",
-                101 => "Switching Protocols",
-                200 => "OK",
-                201 => "Created",
-                202 => "Accepted",
-                203 => "Non-Authoritative Information",
-                204 => "No Content",
-                205 => "Reset Content",
-                206 => "Partial Content",
-                300 => "Multiple Choices",
-                301 => "Moved Permanently",
-                302 => "Found",
-                303 => "See Other",
-                304 => "Not Modified",
-                305 => "Use Proxy",
-                307 => "Temporary Redirect",
-                400 => "Bad Request",
-                401 => "Unauthorized",
-                402 => "Payment Required",
-                403 => "Forbidden",
-                404 => "Not Found",
-                405 => "Method Not Allowed",
-                406 => "Not Acceptable",
-                407 => "Proxy Authentication Required",
-                408 => "Request Time-out",
-                409 => "Conflict",
-                410 => "Gone",
-                411 => "Length Required",
-                412 => "Precondition Failed",
-                413 => "Request Entity Too Large",
-                414 => "Request-URI Too Large",
-                415 => "Unsupported Media Type",
-                416 => "Requested range not satisfiable",
-                417 => "Expectation Failed",
-                500 => "Internal Server Error",
-                501 => "Not Implemented",
-                502 => "Bad Gateway",
-                503 => "Service Unavailable",
-                504 => "Gateway Time-out"
-            );
-
-            if (array_key_exists($status, $codes)) {
-                header("HTTP/1.1: {$status} {$codes[$status]}");
-            }
-        } else {
-            // check valid header
-            if (preg_match("/^.+\:\s.+$/", $status)) {
-                header($status);
-            }
-        }
-    }
-    // }}}
 
     // {{{ getCurrentForwardName()
     /**
@@ -257,23 +174,6 @@ class Ethna_ViewClass
         else {
             return false;
         }
-    }
-    // }}}
-
-    // {{{ error 
-    /**
-     *  エラーページ出力用のHTTPステータスコードを指定します。
-     *
-     *  @param  int  HTTPステータスコード
-     *  @access public
-     */
-    public function error($code)
-    {
-        $this->header($code);
-
-        // template 以下に error404.tpl とかがあれば， 
-        // preforward で $this->error(404); とかすればいい
-        $this->forward_path = "error{$code}.tpl";
     }
     // }}}
 
